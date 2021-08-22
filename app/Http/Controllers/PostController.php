4934f88199298df230a11076a232ec7713602cb4
Ankit9835 <ankydchrismatic@gmail.com>
2021-08-22 15:55:05 +0530

    Upto CRUD Operation Completed

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+    public function index(){
+      return Post::all();
+    }
+
+    public function store(Request $request){
+        try{
+
+            $post = new Post;
+            $post->title = $request->title;
+            $post->body = $request->body;
+            if($post->save()){
+                return response()->json(['status' => 'success', 'message' => 'Post Created SuccessFully']);
+            }
+        } catch(\Exception $e){
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function update(Request $request, $id){
+        try{
+
+            $post = Post::findOrFail($id);
+            $post->title = $request->title;
+            $post->body = $request->body;
+            if($post->save()){
+                return response()->json(['status' => 'success', 'message' => 'Post Updated SuccessFully']);
+            }
+        } catch(\Exception $e){
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function destroy($id){
+         try{
+
+            $post = Post::findOrFail($id);
+            if($post->delete()){
+                return response()->json(['status' => 'success', 'message' => 'Post Deleted SuccessFully']);
+            }
+        } catch(\Exception $e){
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+}
